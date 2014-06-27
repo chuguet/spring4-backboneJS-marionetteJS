bb3475266d290c02e415c93b6a38a438c100beee
huguet10@gmail.com <huguet10@gmail.com@56331ead-5f2d-f8d1-0217-ad272cf1ce31>
2014-06-27 07:09:34 +0000

    Se añade log

--- trunk/web/src/main/java/com/amaris/web/controller/PersonaController.java
+++ trunk/web/src/main/java/com/amaris/web/controller/PersonaController.java
@@ -1,5 +1,7 @@
 package com.amaris.web.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,27 +14,34 @@ import com.amaris.web.beans.Persona;
 @Controller
 public class PersonaController {
 
+	private final static Log LOG = LogFactory.getLog(PersonaController.class);
+
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
 	public @ResponseBody
 	Mensaje doPost(@RequestBody Persona persona) {
+		LOG.info("Post al server con los datos " + persona.getName() + " "
+				+ persona.getTlf());
 		Mensaje result = new Mensaje();
 		result.setMensaje(persona.getName() + " " + persona.getTlf());
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String printHome() {
-           return "index";
-    }
-	
+	public String printHome() {
+		LOG.info("Ir a pagina de home");
+		return "index";
+	}
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String printRoot() {
-           return "index";
-    }
+	public String printRoot() {
+		LOG.info("Ir a pagina de home");
+		return "index";
+	}
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String test() {
-           return "testRunner";
-    }
+	public String test() {
+		LOG.info("Ir a pagina de tests");
+		return "testRunner";
+	}
 
 }
